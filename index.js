a716380870818fcb3745f1f0af89457c232e0bc6
Olivier van den Biggelaar <ovdbigge@gmail.com>
2020-06-24 21:50:15 +0100

    Adding CL arguments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Usage: node index.js
-
 require("dotenv").config();
 const axios = require("axios").default;
 const Web3 = require("web3");
@@ -7,23 +5,31 @@ const fs = require("fs");
 const { pick, pickBy } = require("ramda");
 const { inspect } = require("util");
 
+// Can be overridden by command line arguments
 const MODULE = "TransferManager";
-// const VERSION = "1.4.5";
 const VERSION = "1.6.0";
 const WALLET = "0xc4d46ecbc83f41d0bf71a39868d3f830299068b8";
 const METHOD = "addModule";
+const FROM_BLOCK = "10000000";
+const TO_BLOCK = "20000000";
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`));
-const transferMgrAbi = JSON.parse(fs.readFileSync(`${__dirname}/abi/${VERSION}/${MODULE}.json`)).abi;
 
-function decodeInput(input, abi = transferMgrAbi) {
-  const fun = abi.find((e) => web3.eth.abi.encodeFunctionSignature(e) === input.slice(0, 10));
-  if (fun) {
-    const decodedInput = web3.eth.abi.decodeParameters(fun.inputs, input.slice(10));
-    const cleanedInput = pickBy((val, key) => key !== "__length__" && isNaN(parseInt(key)), { ...decodedInput });
-    return { funName: fun.name, funInput: cleanedInput };
-  }
-  return null;
+function parseCommandLineArgs() {
+  let idx;
+  idx = process.argv.indexOf("--module");
+  const moduleName = idx > -1 ? process.argv[idx + 1] : MODULE;
+  idx = process.argv.indexOf("--wallet");
+  const wallet = idx > -1 ? process.argv[idx + 1] : WALLET;
+  idx = process.argv.indexOf("--version");
+  const version = idx > -1 ? process.argv[idx + 1] : VERSION;
+  idx = process.argv.indexOf("--method");
+  const method = idx > -1 ? process.argv[idx + 1] : METHOD;
+  idx = process.argv.indexOf("--from");
+  const fromBlock = idx > -1 ? process.argv[idx + 1] : FROM_BLOCK;
+  idx = process.argv.indexOf("--to");
+  const toBlock = idx > -1 ? process.argv[idx + 1] : TO_BLOCK;
+  return { moduleName, wallet, version, method, fromBlock, toBlock };
 }
 
 async function getModuleAddress(moduleName, version) {
@@ -35,11 +41,17 @@ async function getModuleAddress(moduleName, version) {
   return versions.find((v) => v.version === version).modules.find((m) => m.name === moduleName).address;
 }
 
-async function getFunctionInfo(log) {
+function decimalizeValues(log) {
+  const toDecimalize = pick(["timeStamp", "blockNumber", "gasPrice", "gasUsed", "logIndex", "transactionIndex"], log);
+  const decimalized = Object.fromEntries(Object.entries(toDecimalize).map(([k, v]) => [k, parseInt(web3.utils.hexToNumberString(v))]));
+  return decimalized;
+}
+
+async function getFunctionInfo(log, abi) {
   const { input } = await web3.eth.getTransaction(log.transactionHash);
-  const fun = decodeInput(input);
+  const fun = decodeInput(input, abi);
   if (fun && fun.funName === "execute") {
-    const subfun = decodeInput(fun.funInput._data);
+    const subfun = decodeInput(fun.funInput._data, abi);
     if (subfun) {
       fun.subFunName = subfun.funName;
       fun.subFunInput = subfun.funInput;
@@ -49,13 +61,17 @@ async function getFunctionInfo(log) {
   return fun;
 }
 
-function decimalizeValues(log) {
-  const toDecimalize = pick(["timeStamp", "blockNumber", "gasPrice", "gasUsed", "logIndex", "transactionIndex"], log);
-  const decimalized = Object.fromEntries(Object.entries(toDecimalize).map(([k, v]) => [k, parseInt(web3.utils.hexToNumberString(v))]));
-  return decimalized;
+function decodeInput(input, abi) {
+  const fun = abi.find((e) => web3.eth.abi.encodeFunctionSignature(e) === input.slice(0, 10));
+  if (fun) {
+    const decodedInput = web3.eth.abi.decodeParameters(fun.inputs, input.slice(10));
+    const cleanedInput = pickBy((val, key) => key !== "__length__" && isNaN(parseInt(key)), { ...decodedInput });
+    return { funName: fun.name, funInput: cleanedInput };
+  }
+  return null;
 }
 
-function decodeTopics(log, abi = transferMgrAbi) {
+function decodeTopics(log, abi) {
   const eventAbi = abi.find((e) => e.type === "event" && web3.eth.abi.encodeEventSignature(e) === log.topics[0]);
   const decoded = web3.eth.abi.decodeLog(eventAbi.inputs, log.data, log.topics);
   const cleaned = pickBy((val, key) => key !== "__length__" && isNaN(parseInt(key)), { ...decoded });
@@ -63,7 +79,9 @@ function decodeTopics(log, abi = transferMgrAbi) {
 }
 
 async function main() {
-  const moduleAddress = await getModuleAddress(MODULE, VERSION);
+  const { moduleName, wallet, version, method, fromBlock, toBlock } = parseCommandLineArgs();
+
+  const moduleAddress = await getModuleAddress(moduleName, version);
   const etherscan = axios.create({
     baseURL: "https://api.etherscan.io",
     timeout: 10000,
@@ -75,23 +93,25 @@ async function main() {
         module: "logs",
         action: "getLogs",
         apikey: process.env.ETHERSCAN_API_KEY,
-        fromBlock: "10320000",
-        toBlock: "21000000",
+        fromBlock: fromBlock,
+        toBlock: toBlock,
         address: moduleAddress,
-        topic1: web3.utils.padLeft(WALLET, 64),
+        topic1: web3.utils.padLeft(wallet, 64),
       },
     })
   ).data.result;
 
+  const moduleAbi = JSON.parse(fs.readFileSync(`${__dirname}/abi/${version}/${moduleName}.json`)).abi;
+
   const decoded = await Promise.all(
     logs.map(async (log) => {
-      const fun = await getFunctionInfo(log);
+      const fun = await getFunctionInfo(log, moduleAbi);
       const decimalized = decimalizeValues(log);
-      const decodedTopics = decodeTopics(log);
+      const decodedTopics = decodeTopics(log, moduleAbi);
       return { ...log, ...decodedTopics, ...fun, ...decimalized };
     })
   );
-  const filtered = decoded.filter((e) => e.subFunName === METHOD || e.funName === METHOD);
+  const filtered = decoded.filter((e) => e.subFunName === method || e.funName === method);
   console.log(inspect(filtered, { colors: true, depth: 2 }));
 }
 
