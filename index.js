2adb0dca6d118e88123ce8e748a370b377d411b7
Olivier van den Biggelaar <ovdbigge@gmail.com>
2020-06-24 21:07:31 +0100

    Simplification

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const { pick, pickBy } = require("ramda");
 const { inspect } = require("util");
 
 const MODULE = "TransferManager";
-const VERSION = "1.4.5";
+// const VERSION = "1.4.5";
+const VERSION = "1.6.0";
 const WALLET = "0xc4d46ecbc83f41d0bf71a39868d3f830299068b8";
 const METHOD = "addModule";
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`));
-const rawTransferMgrAbi = JSON.parse(fs.readFileSync(`${__dirname}/abi/${VERSION}/${MODULE}.json`)).abi;
-const transferMgrAbi = rawTransferMgrAbi.filter((e) => e.type === "function" || e.type === "event");
+const transferMgrAbi = JSON.parse(fs.readFileSync(`${__dirname}/abi/${VERSION}/${MODULE}.json`)).abi;
 
 function decodeInput(input, abi = transferMgrAbi) {
   const fun = abi.find((e) => web3.eth.abi.encodeFunctionSignature(e) === input.slice(0, 10));
@@ -31,13 +31,7 @@ async function getModuleAddress(moduleName, version) {
     baseURL: process.env.MODULE_ENDPOINT,
     timeout: 10000,
   });
-
-  const { versions } = (
-    await backend.get("", {
-      params: {},
-    })
-  ).data;
-
+  const { versions } = (await backend.get()).data;
   return versions.find((v) => v.version === version).modules.find((m) => m.name === moduleName).address;
 }
 
